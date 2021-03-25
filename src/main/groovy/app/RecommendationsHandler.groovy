982886259da05cc6a8324838d06b3fe195eeed7d
Ibrahim.H <bitsnaps@yahoo.fr>
2021-03-25 18:20:13 +0100

    optimize recommendations handler by senting parallel requests

--- src/main/groovy/app/RecommendationsHandler.groovy
+++ src/main/groovy/app/RecommendationsHandler.groovy
@@ -2,6 +2,7 @@ package app
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import ratpack.exec.Promise
+import ratpack.exec.util.ParallelBatch
 import ratpack.exec.util.SerialBatch
 import ratpack.handling.Context
 import ratpack.handling.Handler
@@ -9,6 +10,7 @@ import ratpack.http.client.HttpClient
 import ratpack.http.client.ReceivedResponse
 
 import javax.inject.Inject
+import java.time.Duration
 
 import static ratpack.jackson.Jackson.json
 
@@ -33,7 +35,8 @@ class RecommendationsHandler implements Handler {
     @Override
     void handle(Context ctx) throws Exception {
         def promises = IDS.collect { id -> prepareHttpConnection(id) }
-        SerialBatch.of(promises)
+//      We use ParallelBatch which is much faster than SerialBatch for parallel request handling
+        ParallelBatch.of(promises)
             .yield()
             .then({ def products ->
                 ctx.render(json( products ))
@@ -41,11 +44,15 @@ class RecommendationsHandler implements Handler {
     }
 
     private Promise<Product> prepareHttpConnection(String id){
-        return http.get("http://localhost:5050/products/${id}".toURI()).mapIf(
+        return http.get("http://localhost:5050/products/${id}".toURI(), { def request ->
+            request.readTimeout(Duration.ofMillis(500))
+        }).mapIf(
                 { def response -> response.status.is2xx() },
                 { def response -> mapper.readValue(response.body.inputStream, Product) },
                 { _ -> null}
-        )
+        ).onError { def error ->
+            println(error.message)
+        }
     }
 
 }
