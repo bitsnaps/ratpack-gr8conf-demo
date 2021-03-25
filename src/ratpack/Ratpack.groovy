ee6662584a32f28189447e2e7b47242a05a726e2
Ibrahim.H <bitsnaps@yahoo.fr>
2021-03-25 17:10:47 +0100

    add a simple blocking request handler

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,18 +1,35 @@
-import ratpack.groovy.template.MarkupTemplateModule
+import app.FakeRemoteProductService
+import app.Product
+import app.ProductService
+import static ratpack.jackson.Jackson.json
 
-import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
+
+  serverConfig {
+    threads(1)
+    development(false)
+  }
+
   bindings {
-    module MarkupTemplateModule
+    // instruct Guice for DI (like: whenever I try to inject ProductService do inject FakeRemoteProductService
+    bind(ProductService, FakeRemoteProductService)
   }
 
   handlers {
-    get {
-      render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
+
+   get('products/:id') { ProductService productService ->
+     final Product product = productService.findProductById(pathTokens.id)
+     if (product){
+       render json(product)
+     } else {
+       response.status(404)
+       render json([message: 'Not found'])
+     }
+
     }
 
-    files { dir "public" }
+//    files { dir "public" }
   }
 }
